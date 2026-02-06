57acba0f01437ad3b936592714b9582913adf51a
okash1n <48118431+okash1n@users.noreply.github.com>
2026-02-06 23:20:49 +0900

    ## home-managerへ引数連携
    - extraSpecialArgsでusernameを渡す\n- home moduleの評価エラーを解消

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit username; };
             home-manager.users.${username} = import ./home/default.nix;
           }
         ];
