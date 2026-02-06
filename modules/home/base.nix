f26efc7ad4ff93f4fb16cc3dde75c9b2a7f0f58f
okash1n <48118431+okash1n@users.noreply.github.com>
2026-02-07 00:25:48 +0900

    ## 非対話zshでの補完初期化を抑制

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -33,21 +33,23 @@
     setopt INC_APPEND_HISTORY
     setopt AUTO_CD
 
-    autoload -Uz compinit
-    compinit
+    if [[ -o interactive ]]; then
+      autoload -Uz compinit
+      compinit
 
-    if [ -f "${pkgs.fzf}/share/fzf/key-bindings.zsh" ]; then
-      source "${pkgs.fzf}/share/fzf/key-bindings.zsh"
+      if [ -f "${pkgs.fzf}/share/fzf/key-bindings.zsh" ]; then
+        source "${pkgs.fzf}/share/fzf/key-bindings.zsh"
+      fi
+      if [ -f "${pkgs.fzf}/share/fzf/completion.zsh" ]; then
+        source "${pkgs.fzf}/share/fzf/completion.zsh"
+      fi
+
+      source "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+      source "${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+      source "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
+
+      [[ -f "$HOME/.config/zsh/.p10k.zsh" ]] && source "$HOME/.config/zsh/.p10k.zsh"
     fi
-    if [ -f "${pkgs.fzf}/share/fzf/completion.zsh" ]; then
-      source "${pkgs.fzf}/share/fzf/completion.zsh"
-    fi
-
-    source "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-    source "${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-    source "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
-
-    [[ -f "$HOME/.config/zsh/.p10k.zsh" ]] && source "$HOME/.config/zsh/.p10k.zsh"
   '';
 
   home.file.".config/zsh/.p10k.zsh".text = ''
